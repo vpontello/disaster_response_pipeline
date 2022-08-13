c99ad1ab85229946eecafdda7ee61a540811bfa4
Victor Pontello <vpontello@outlook.com>
2022-08-13 12:13:35 -0300

    fine adjustments on training notebooks script

--- src/models/train_classifier.py
+++ src/models/train_classifier.py
@@ -226,7 +226,7 @@ def main():
         print('Please provide the filepath of the disaster messages database '\
               'as the first argument and the filepath of the pickle file to '\
               'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+              'train_classifier.py DisasterResponse.db LGBMClassifier.pkl')
 
 
 if __name__ == '__main__':
