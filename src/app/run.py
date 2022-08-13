a55f69edd0cce9cc335ea0eccc9d402a3425969b
Victor Pontello <vpontello@outlook.com>
2022-08-13 15:04:14 -0300

    adjustments on app

--- src/app/run.py
+++ src/app/run.py
@@ -8,7 +8,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../../data/disaster.db')
+engine = create_engine('sqlite:///../../data/02_trusted/DisasterResponse.db')
 df = pd.read_sql_table('messages_dataset', engine)
 
 # load model
-model = joblib.load("../../models/out/LGBMClassifier.pkl")
+model = joblib.load("../../data/03_models/out/LGBMClassifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
